51a5a837328a0c3497b0cbf1d813e04ba6e11bd2
Vishwanath-Eswaran <139625497+Vishwanath-Eswaran@users.noreply.github.com>
2023-07-17 23:05:18 +0530

    Update rupeename.py

--- rupeename.py
+++ rupeename.py
@@ -33,11 +33,11 @@ below_ten={
     "18":"Eighteen",
     "19":"Nineteen"
 }
-n=int(input("Enter the amount you want to convert into text:"))
-nu=[*str(n)]
+n=int(input("Enter the amount you want to convert into text:"))#1000
+nu=[*str(n)]#["1","0","0","0"]
 maxi=""
 l=len(nu)
-def grouping(x):
+def grouping(x:list):
     a=len(nu)
     if a==1:
         o=x[-2:][-1]
@@ -145,6 +145,8 @@ def co(x:str):
         text=below_ten[x]
     elif int(x)<10 and int(x)>0:
         text=ones[str(int(x))]
+    elif int(x)==0:
+        tect=ones["0"]
     else:
         o=ones[y[1]]
         t=tens[y[0]]
@@ -160,23 +162,74 @@ def number(c,l,th,h,o):
     
     if ic==0 and il==0 and ith==0 and ih==0:
         num=o
-    elif ic==0 and il==0 and ith==0:
+    elif ic==0 and il==0 and ith==0 and io!=0:
         num=co(h)+" hundred and "+co(o)
-    elif ic==0 and il==0:
+    elif ic==0 and il==0 and io==0 and ith==0:
+        num=co(h)+" hundred"
+    elif ic==0 and il==0 and io!=0 and ih!=0:
         num=co(th)+" thousand "+co(h)+" hundred and "+co(o)
-    elif ic==0 :
+    elif ic==0 and il==0 and io==0 and ith!=0:
+        num=co(th)+" thousand "+co(h)+" hundred"
+    elif ic==0 and il==0 and io!=0 and ih==0 and ith!=0:
+        num=co(th)+" thousand and "+co(o)
+    elif ic==0 and il!=0 and ith!=0 and ih!=0 and io!=0:#lakhs
         num=co(l)+" lakh "+co(th)+" thousand "+co(h)+" hundred and "+co(o)
-    else:
+    elif ic==0 and il!=0 and ith==0 and ih==0 and io==0:
+        num=co(l)+" lakh "
+    elif ic==0 and il!=0 and ith!=0 and ih==0 and io==0:
+        num=co(l)+" lakh "+co(th)+" thousand "
+    elif ic==0 and il!=0 and ith==0 and ih!=0 and io==0:
+        num=co(l)+" lakh "+co(h)+" hundred"
+    elif ic==0 and il!=0 and ith==0 and ih==0 and io!=0:
+        num=co(l)+" lakh "+co(o)
+    elif ic==0 and il!=0 and ith!=0 and ih!=0 and io==0:
+        num=co(l)+" lakh "+co(th)+" thousand "+co(h)+" hundred"
+    elif ic==0 and il!=0 and ith==0 and ih!=0 and io!=0:
+        num=co(l)+" lakh "+co(h)+" hundred and "+co(o)
+    elif ic==0 and il!=0 and ith!=0 and ih==0 and io!=0:
+        num=co(l)+" lakh "+co(th)+" thousand "+co(o)
+    elif ic!=0 and il!=0 and ith!=0 and ih!=0 and io!=0:#crore 1
         num=co(c)+" crore "+co(l)+" lakh "+co(th)+" thousand "+co(h)+" hundred and "+co(o)
-    return(num)
+    elif ic!=0 and il==0 and ith==0 and ih==0 and io==0:#crore 2
+        num=co(c)+" crore "
+    elif ic!=0 and il!=0 and ith==0 and ih==0 and io==0:#crore 3
+        num=co(c)+" crore "+co(l)+" lakh "
+    elif ic!=0 and il==0 and ith!=0 and ih==0 and io==0:#crore 4
+        num=co(c)+" crore "+co(l)+co(th)+" thousand "
+    elif ic!=0 and il==0 and ith==0 and ih!=0 and io==0:#crore 5
+        num=co(c)+" crore "+co(h)+" hundred"
+    elif ic!=0 and il==0 and ith==0 and ih==0 and io!=0:#crore 6
+        num=co(c)+" crore "+co(o)
+    elif ic!=0 and il!=0 and ith!=0 and ih==0 and io==0:#crore 7
+        num=co(c)+" crore "+co(l)+" lakh "+co(th)+" thousand "
+    elif ic!=0 and il==0 and ith==0 and ih!=0 and io!=0:#crore 8
+        num=co(c)+" crore "+co(h)+" hundred and "+co(o)
+    elif ic!=0 and il!=0 and ith==0 and ih!=0 and io==0:#crore 9
+        num=co(c)+" crore "+co(l)+" lakh "+co(h)+" hundred"
+    elif ic!=0 and il==0 and ith!=0 and ih==0 and io!=0:#crore 10
+        num=co(c)+" crore "+co(th)+" thousand "+co(o)
+    elif ic!=0 and il!=0 and ith==0 and ih==0 and io!=0:#crore 11
+        num=co(c)+" crore "+co(l)+" lakh "+co(o)
+    elif ic!=0 and il==0 and ith!=0 and ih!=0 and io==0:#crore 12
+        num=co(c)+" crore "+co(th)+" thousand "+co(h)+" hundred"
+    elif ic!=0 and il!=0 and ith!=0 and ih!=0 and io==0:#crore 13
+        num=co(c)+" crore "+co(l)+" lakh "+co(th)+" thousand "+co(h)+" hundred"
+    elif ic!=0 and il==0 and ith!=0 and ih!=0 and io!=0:#crore 14
+        num=co(c)+" crore "+co(th)+" thousand "+co(h)+" hundred and "+co(o)
+    elif ic!=0 and il!=0 and ith==0 and ih!=0 and io!=0:#crore 15
+        num=co(c)+" crore "+co(l)+" lakh "+co(h)+" hundred and "+co(o)
+    elif ic!=0 and il!=0 and ith!=0 and ih==0 and io!=0:#crore 16
+        num=co(c)+" crore "+co(l)+" lakh "+co(th)+" thousand "+co(o)
+
+    return(num)# type:ignore
 
 
 if n<20 and n>10:
-    print(below_ten[str(n)])
+    print(f"₹{n} -> Rs.",below_ten[str(n)])
 elif n<10 and n>0:
-    print(ones[str(n)])
+    print(f"₹{n} -> Rs.",(ones[str(n)]))
 elif n==0:
-    print("zero")
+    print(f"₹{n} -> Rs.","zero")
 else:
     nu=grouping(nu)
     crore= (nu[0])
@@ -185,4 +238,3 @@ else:
     hundred=(nu[3])
     one=(nu[4])
     print(f"₹{n} -> Rs.",number(crore,lak,thousand,hundred,one))
-input()
